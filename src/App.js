ce4ccd4b5436d4c36d01bf962198137bf0226472
Abhi Varde <91776428+AbhiVarde@users.noreply.github.com>
2023-06-16 10:04:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,76 +1,76 @@
 import React from "react";
-import ChatBot from 'react-simple-chatbot';
+import ChatBot from "react-simple-chatbot";
 
-const steps=[
+const steps = [
   {
-    id: '0',
-    message: 'Hello there!👋',
-    trigger: '1',
+    id: "0",
+    message: "Hello there!👋",
+    trigger: "1",
   },
   {
-    id: '1',
+    id: "1",
     message: "I'm Tommy Bot,your smart assistant.",
-    trigger: '2',
+    trigger: "2",
   },
   {
-    id: '2',
+    id: "2",
     user: true,
-    trigger: '3',
+    trigger: "3",
   },
   {
-    id: '3',
-    message: 'Please provide your name!',
-    trigger: '4',
-  },{
-    id: '4',
+    id: "3",
+    message: "Please provide your name!",
+    trigger: "4",
+  },
+  {
+    id: "4",
     user: true,
-    trigger: '5',
-  },{
-    id: '5',
-    message: 'Hi {previousValue}, nice to meet you!',
-    trigger: '6',
-  },{
-    id: '6',
+    trigger: "5",
+  },
+  {
+    id: "5",
+    message: "Hi {previousValue}, nice to meet you!",
+    trigger: "6",
+  },
+  {
+    id: "6",
     message: "Let's Guess! How old I am?",
-    trigger: '7',
+    trigger: "7",
   },
   {
-    id: '7',
+    id: "7",
     options: [
-      { value: 1, label: '18', trigger: '8' },
-      { value: 2, label: '22', trigger: '8' },
-      { value: 3, label: '20', trigger: '9' },
+      { value: 1, label: "18", trigger: "8" },
+      { value: 2, label: "22", trigger: "8" },
+      { value: 3, label: "20", trigger: "9" },
     ],
   },
   {
-    id: '8',
-    message: 'Wrong answer, try again.',
-    trigger: '7',
+    id: "8",
+    message: "Wrong answer, try again.",
+    trigger: "7",
   },
   {
-    id: '9',
-    message: 'Awesome! You are a telepath!',
-    trigger: '10',
-  },{
-    id: '9',
-    message: 'Awesome! You are a telepath!',
-    trigger: '10',
-  },{
-    id: '10',
+    id: "9",
+    message: "Awesome! You are a telepath!",
+    trigger: "10",
+  },
+  {
+    id: "10",
     message: "Please Rate! How you Enjoy!🤩",
-    trigger: '11',
+    trigger: "11",
   },
   {
-    id: '11',
+    id: "11",
     options: [
-      { value: 1, label: 'Poor', trigger: '12' },
-      { value: 2, label: 'Good', trigger: '12' },
-      { value: 3, label: 'Excellent', trigger: '12' },
+      { value: 1, label: "Poor", trigger: "12" },
+      { value: 2, label: "Good", trigger: "12" },
+      { value: 3, label: "Excellent", trigger: "12" },
     ],
   },
   {
-    id: '12',
-    message: 'Thanks! Have a Great Day!😇',
+    id: "12",
+    message: "Thanks! Have a Great Day!😇",
     end: true,
   },
 ];
@@ -78,16 +78,13 @@ const steps=[
 function App() {
   return (
     <div className="App">
-        <center> 
-          <h1>Hey there! Welcome to ChatBot.</h1>
-          <p>I'm Abhi Varde.Here to Help😊!</p>
-            <ChatBot 
-              headerTitle = "Tommy Bot"
-              steps={steps}
-            />
-        </center>
+      <center>
+        <h1>Hey there! Welcome to ChatBot App.</h1>
+        <p>I'm Abhi Varde.Here to Help😊!</p>
+        <ChatBot headerTitle="Tommy Bot" steps={steps} />
+      </center>
     </div>
   );
 }
- 
+
 export default App;
